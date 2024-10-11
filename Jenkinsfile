485a150abe4cf4546e5f20749767c40917b432ee
rimas123-creator <caderrimas1999@gmail.com>
2024-10-11 23:27:36 +0530

    update: SeleniumTest.java file updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,37 +1,26 @@
 pipeline {
     agent any
 
-    tools {
-        maven 'maven'  // Ensure Maven is installed on Jenkins and available
-    }
-
     stages {
-        stage('Checkout Code') {
+        stage('Checkout') {
             steps {
-                // Checkout the code from the repository
                 git 'https://github.com/rimas123-creator/selenium-jenkins.git'
             }
         }
-
-        stage('Build') {
+        stage('Install Dependencies') {
             steps {
-                // Build the project and run tests
                 sh 'mvn clean install'
             }
         }
-
         stage('Run Selenium Tests') {
             steps {
-                // Run the tests using Maven Surefire plugin
                 sh 'mvn test'
             }
         }
     }
-
     post {
         always {
-            // Publish test results (JUnit format)
-            junit 'target/surefire-reports/*.xml'
+            junit '**/target/surefire-reports/*.xml'
         }
     }
 }
--- src/test/java/com/example/SeleniumTest.java
+++ src/test/java/com/example/SeleniumTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.Assert.assertEquals;
@@ -16,27 +15,14 @@ public class SeleniumTest {
 
     @Before
     public void setUp() {
-        // Setup WebDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
-
-        // Set Chrome options for headless mode
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Run Chrome in headless mode
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
-
-        // Initialize ChromeDriver with options
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @Test
     public void testGoogleTitle() {
-        // Navigate to Google
         driver.get("https://www.google.com");
-
-        // Assert title
-        String pageTitle = driver.getTitle();
-        assertEquals("Google", pageTitle);
+        assertEquals("Google", driver.getTitle());
     }
 
     @After
